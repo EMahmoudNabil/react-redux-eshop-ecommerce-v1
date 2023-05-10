d9401c90e0bb0bdb8271e6458af9a479e855c9db
Mahmoud Nabil <e.mahmoudnabil@gmail.com>
2023-05-10 22:12:45 +0300

    upload project v1

---
+++ src/hook/products/view-products-detalis-hook.js
@@ -0,0 +1,99 @@
+import React, {
+    useEffect
+} from 'react'
+import {
+    useDispatch,
+    useSelector
+} from "react-redux";
+import {
+    getOneProduct,
+    getProductLike,
+    getProductYouLike
+} from '../../redux/actions/productsAction';
+import mobile from '../../images/mobile.png'
+import {
+    getOneCategory
+} from '../../redux/actions/categoryAction';
+import {
+    getOneBrand
+} from '../../redux/actions/brandAction';
+const ViewProductsDetalisHook = (prodID) => {
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getOneProduct(prodID))
+    }, [])
+
+    const oneProducts = useSelector((state) => state.allproducts.oneProduct)
+    const oneCategory = useSelector((state) => state.allCategory.oneCategory)
+    const oneBrand = useSelector((state) => state.allBrand.oneBrand)
+    const productLike = useSelector((state) => state.allproducts.productLike)
+    //to show products item
+    let item = [];
+    if (oneProducts && oneProducts.data) {
+        item = oneProducts.data;
+
+    } else
+        item = []
+
+    const get = async () => {
+        if (oneProducts && oneProducts.data) {
+            item = oneProducts.data;
+        if (item.category)
+            await dispatch(getOneCategory(item.category._id))
+        if (item.brand)
+            await dispatch(getOneBrand(item.brand))
+        if (item.category)
+            await dispatch(getProductLike(item.category._id))
+        }
+    }
+
+
+
+    useEffect(() => {
+        get()
+
+    }, [])
+
+
+    //to view images gallery
+    let images = []
+    if (item.images)
+        images = item.images.map((img) => {
+            return {
+                original: img
+            }
+        })
+    else {
+        images = [{
+            original: `${mobile}`
+        }]
+    }
+
+
+    //to show category item
+    let cat = [];
+    if (oneCategory && oneCategory.data)
+        cat = oneCategory.data;
+    else
+        cat = []
+
+    //to show brand item
+    let brand = [];
+    if (oneBrand && oneBrand.data)
+        brand = oneBrand.data;
+    else
+        brand = []
+
+    let prod = []
+    if (productLike)
+        prod = productLike.data;
+    else
+        prod = []
+        
+
+    
+    return [item, images, cat, brand, prod]
+}
+
+export default ViewProductsDetalisHook
